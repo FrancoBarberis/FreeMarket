6e927dcb326f2e8c2de06cdd1917bbadaeb8a5fc
Franco Barberis <francobarberissic3@gmail.com>
2025-09-13 13:26:31 -0300

    Añadido Vite

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,3 +6,5 @@ function App() {
     <Header />
   </div>;
 }
+
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function Header() {
   return <header>
-    <div className="logo"></div>
+    <div className="logo">LOGO</div>
+    <div className="buscador">BUSCADOR</div>
+    <div className="banner">BANNER</div>
   </header>;
 }
 
