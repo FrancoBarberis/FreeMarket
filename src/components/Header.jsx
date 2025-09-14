bc6f2830f62da1cefe962ec3869be3d6c889fd84
Franco Barberis <francobarberissic3@gmail.com>
2025-09-14 13:01:08 -0300

    Estructura básica Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function Header() {
-  return <header>
+  return <header className="header">
     <div className="logo">LOGO</div>
-    <div className="buscador">BUSCADOR</div>
-    <div className="banner">BANNER</div>
+    <div className="buscador"><input type="text" placeholder="Buscar productos, marcas, etc..."/></div>
+    <div>BANNER</div>
+    <div>ENVIAR A</div>
+    <ul>
+      <li><a href="" className="navItem">CREAR CUENTA</a></li>
+      <li><a href="" className="navItem">INGRESAR</a></li>
+      <li><a href="" className="navItem">MIS COMPRAS</a></li>
+    </ul>
+    <a href=""><i class="fa-thin fa-cart-shopping"></i></a>
   </header>;
 }
 
