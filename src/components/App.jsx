8434a1159f5f9d4f077728b21fd9611fdf69e344
Franco Barberis <francobarberissic3@gmail.com>
2025-09-15 16:24:47 -0300

    assets, cambios visuales

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./Header";
 
 function App() {
-  return <div className="App">
-    <Header />
-    {/* CARRUSEL */}
-  </div>;
+  return (
+    <div className="App">
+      <Header />
+      {/* CARRUSEL */}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Search from "./Search";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import logo from "../assets/MusimarketLOGO.png";
 
 function Header() {
   return <header className="header">
-    <div className="logo">LOGO</div>
+    <div ><img className="logo" src={logo} alt="logo" /></div>
+    <div className="gridHeader">
     <Search></Search>
-    <div>BANNER</div>
-    <div>ENVIAR A</div>
+      <div>BANNER</div>
     <ul>
       <li><a href="" className="navItem">Crear cuenta</a></li>
       <li><a href="" className="navItem">Ingresar</a></li>
@@ -19,6 +20,8 @@ function Header() {
       <li><a href="" className="navItem">Vender</a></li>
       <li><a href=""><FontAwesomeIcon icon={faShoppingCart}/></a></li>
     </ul>
+    </div>
+    
   </header>;
 }
 
